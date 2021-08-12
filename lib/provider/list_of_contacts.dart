4d4b66d767e5ff808bc31e7c0a116b6d111a36d3
MarcosIsaias <contato.marcosisaias@gmail.com>
2021-08-12 18:52:38 -0300

    Projeto Google Agenda Part 2, Up Arquivos de Contatos!

---
+++ lib/provider/list_of_contacts.dart
@@ -0,0 +1,47 @@
+import 'package:google_agenda/models/contact.dart';
+
+List<Contact> listOfContacts = [
+  Contact(
+    name: "Nicole Soares", 
+    photo: "assets/images/Nicole.jpg", 
+    email: "nicolesoh@gmail.com", 
+    phone: "51 99422-7785"),
+    Contact(
+    name: "Camila Sweth", 
+    photo: "assets/images/Camila.jpg", 
+    email: "cahsweth@gmail.com", 
+    phone: "51 99232-9725",
+    isFavorite: true),
+    Contact(
+    name: "Paulo Brito", 
+    photo: "assets/images/Paulo.jpg", 
+    email: "paulao2@gmail.com", 
+    phone: "51 99432-9985"),
+    Contact(
+    name: "Lucas Satoshi", 
+    photo: "assets/images/Lucas.jpg", 
+    email: "satoshi@gmail.com", 
+    phone: "51 99233-8987",
+    isFavorite: true),
+    Contact(
+    name: "Juliana Silva", 
+    photo: "assets/images/Juliana.jpg", 
+    email: "juh_silva@gmail.com", 
+    phone: "51 99466-7235"),
+    Contact(
+    name: "Thomas Miguel", 
+    photo: "assets/images/Thomas.jpg", 
+    email: "thomasmiguel@gmail.com", 
+    phone: "51 98582-7874",
+    isFavorite: true),
+    Contact(
+    name: "Claudia Ribeiro", 
+    photo: "assets/images/Claudia.jpg", 
+    email: "claudia_rih@gmail.com", 
+    phone: "51 99423-1185"),
+    Contact(
+    name: "Marina Rios", 
+    photo: "assets/images/Marina.jpg", 
+    email: "marina_rios@gmail.com", 
+    phone: "51 98285-7621"),
+];
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_agenda/models/contact.dart';
+import 'package:google_agenda/provider/list_of_contacts.dart';
 import 'package:google_agenda/style.dart';
 
 class Home extends StatefulWidget {
@@ -18,7 +20,7 @@ class _HomeState extends State<Home> {
       ),
       body: ListView.separated(
         itemBuilder: builder,
-        itemCount: 10,
+        itemCount: listOfContacts.length,
         separatorBuilder: (_, index) {
           return Divider();
         },
@@ -27,41 +29,62 @@ class _HomeState extends State<Home> {
   }
 
   Widget builder(BuildContext _, int index) {
+    Contact _contact = listOfContacts.elementAt(index);
     return ListTile(
       leading: IconButton(
-        icon: Icon(Icons.star, color: blueTheme,), 
-        onPressed: () {},),
+        icon: (_contact.isFavorite)
+            ? Icon(
+                Icons.star,
+                color: blueTheme,
+              )
+            : Icon(
+                Icons.star_outline,
+                color: blueTheme,
+              ),
+        onPressed: () {
+          setState(() {
+            _contact.isFavorite = !_contact.isFavorite;
+          });
+        },
+      ),
       trailing: IconButton(
-        icon: Icon(Icons.chevron_right, color: grayTheme,), 
-        onPressed: () {},),
-        title: Row(
-          children: [
-            ClipOval(
-              child: Image.asset
-              ("assets/images/Nicole.jpg", 
-              width: 50, 
+        icon: Icon(
+          Icons.chevron_right,
+          color: grayTheme,
+        ),
+        onPressed: () {},
+      ),
+      title: Row(
+        children: [
+          ClipOval(
+            child: Image.asset(
+              _contact.photo,
+              width: 50,
               height: 50,
               fit: BoxFit.cover,
-              ),
             ),
-            SizedBox(width: 20,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Nicole Soares", 
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _contact.name,
                 style: TextStyle(
-                  fontWeight: FontWeight.bold, 
-                  fontSize: 14, 
-                  color: grayTheme),
-                ),
-                Text("51 99422-7785", 
-                style: TextStyle(
-                  fontSize: 10),
-                ),
-              ],
-            )
-          ],
-        ),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: grayTheme),
+              ),
+              Text(
+                _contact.phone,
+                style: TextStyle(fontSize: 10),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
